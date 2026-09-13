013e190b59897a36180fae19013a4b786f457739
MulaMediaMarketing <58248935+MulaMediaMarketing@users.noreply.github.com>
2026-09-13 13:55:48 -0400

    feat(firstboot): add native controller-first setup wizard

---
+++ include/FirstBootWizard.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "FirstBootService.h"
+#include <windows.h>
+#include <string>
+
+namespace zero {
+
+class FirstBootWizard {
+public:
+    FirstBootWizard(HINSTANCE instance, FirstBootService& service);
+    bool Run();
+
+private:
+    enum class Step { Welcome, Profile, Controller, Display, Audio, Complete };
+
+    HINSTANCE instance_{};
+    HWND hwnd_{};
+    FirstBootService& service_;
+    FirstBootState state_{};
+    Step step_{Step::Welcome};
+    bool finished_{false};
+    bool accepted_{false};
+    unsigned volume_{80};
+    WORD previousButtons_{0};
+
+    static LRESULT CALLBACK WindowProc(HWND, UINT, WPARAM, LPARAM);
+    LRESULT HandleMessage(UINT, WPARAM, LPARAM);
+    bool InitWindow();
+    void EnterFullscreen();
+    void Paint();
+    void PollController();
+    void Advance();
+    void Back();
+    void Complete();
+    void DrawCentered(HDC dc, const std::wstring& text, int y, int height, HFONT font, COLORREF color);
+};
+
+} // namespace zero
