ac1c7b1bed9504147d4e8800e3a27d7266e977fb
MulaMediaMarketing <58248935+MulaMediaMarketing@users.noreply.github.com>
2026-09-16 12:22:39 -0400

    Implement ZERO Player composition root

---
+++ src/ApplicationServices.cpp
@@ -0,0 +1,13 @@
+#include "ApplicationServices.h"
+#include "PlatformPaths.h"
+
+namespace zero {
+
+ApplicationServices::ApplicationServices(const std::filesystem::path& dataRoot)
+    : registry_(PlatformPaths::LibraryRoot()),
+      importer_(PlatformPaths::LibraryRoot()),
+      captures_(PlatformPaths::CapturesRoot()),
+      identity_(dataRoot),
+      settingsStore_(dataRoot) {}
+
+} // namespace zero
