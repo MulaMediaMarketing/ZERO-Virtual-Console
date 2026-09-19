9d318f279944ecef5f3ef7690f3e1948cfd57691
MulaMediaMarketing <58248935+MulaMediaMarketing@users.noreply.github.com>
2026-09-18 22:33:20 -0400

    Centralize service refresh ownership

--- src/ApplicationServices.cpp
+++ src/ApplicationServices.cpp
@@ -9,7 +9,7 @@ ApplicationServices::ApplicationServices(const std::filesystem::path& dataRoot)
       captures_(PlatformPaths::CapturesRoot()),
       identity_(dataRoot),
       settingsStore_(dataRoot),
-      shellModule_(shell_, registry_, captures_, friends_, store_),
+      shellModule_(shell_),
       serviceState_(registry_, captures_, friends_, store_, runtime_) {}
 
 } // namespace zero
